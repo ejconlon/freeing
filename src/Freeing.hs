794b3d620bee5d49fff7bf97b39d227bd9ebc021
Eric Conlon <ejconlon@gmail.com>
2016-08-07 21:58:05 -0700

    init

---
+++ src/Freeing.hs
@@ -0,0 +1,6 @@
+module Freeing
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
---
+++ test/Spec.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Freeing
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
